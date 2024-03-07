59b6e4501b30210c29f0e968ac0744f33064026d
Bilal Ladoui <bilal.ladoui@kuehne-nagel.com>
2024-03-07 17:55:56 +0200

    more configuration

--- main.tf
+++ main.tf
@@ -27,4 +27,38 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.mybucket.id
   acl    = "public-read"
 }
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
 
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+# resource "aws_s3_object" "profile" {
+#   bucket = aws_s3_bucket.mybucket.id
+#   key = "basic.png"
+#   source = "basic.png"
+#   acl = "public-read"
+# }
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.mybucket.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  depends_on = [ aws_s3_bucket_acl.example ]
+}
